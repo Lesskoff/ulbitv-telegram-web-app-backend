e06bad2c6508d7bf4ff199a0ca6bdc4d967500dd
Haseri <lessskoff@gmail.com>
2023-12-20 19:13:10 +0300

    Initial commit

--- index.js
+++ index.js
@@ -84,4 +84,4 @@ app.post("/web-data", async (req, res) => {
 
 const PORT = 8000;
 
-app.listen(PORT, () => console.log("Server started on port" + PORT));
+app.listen(PORT, () => console.log("Server started on port " + PORT));
